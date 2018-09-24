38d9a3e33297f3ff93f4c509d7d6d110d5ee2e4c
Robin Millette <robin@millette.info>
2018-09-24 10:07:49 -0400

    Add Rollo links component.

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,7 @@ import Repo from './repo'
 import Source from './source'
 import GHLink from './gh-link'
 import Graph from './graph'
+import Rollo from './rollo'
+import External from './external'
 
-export { Repo, Source, GHLink, Graph }
+export { Repo, Source, GHLink, Graph, Rollo, External }
---
+++ src/components/rollo.js
@@ -0,0 +1,12 @@
+// npm
+import React from 'react'
+
+// self
+import { External } from '../components'
+
+export default () => (
+  <div>
+    A <External to='http://dev.rollodeqc.com/'>RoLLodeQc</External> project by{' '}
+    <External to='http://robin.millette.info/'>Robin Millette</External>
+  </div>
+)
